3b4685b85045501292e4d9fefa1302bcdf383e80
lunaraquarium <123522570+lunaraquarium@users.noreply.github.com>
2024-05-14 20:03:56 -0500

    Update shared_vs.glsl

--- la_11/shaders/shared_vs.glsl
+++ la_11/shaders/shared_vs.glsl
@@ -1,3 +1,10 @@
+// A standard vertex shader, nothing much else
+//But since you're here, here's a haiku:
+// Yo, why are you here?
+// The great depths of outer space
+// are calling your name!
+// KM, 5/14/24
+
 varying vec2 vUv;
 void main() {
     vUv = vec2(uv.x, uv.y);
